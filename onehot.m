d0a808dacc5605fae4c5f637c1f4c0fefe0329e1
Yunao Fu <104259331+yunaofu@users.noreply.github.com>
2026-09-11 14:50:36 +0800

    Delete onehot.m

--- onehot.m
+++
@@ -1,9 +0,0 @@
-function [ Y_onehot ] = onehot( Y, num_c )
-    num_n = length(Y);
-    Y_onehot = zeros(num_n,num_c);
-    for i=1:num_n  
-        j = Y(i,1);
-        Y_onehot(i,j) = 1;
-    end
-end
-
